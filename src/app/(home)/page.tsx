6d524685d958cca254bdee6c1f7d19d81aef8f74
apophis30 <aditya27may@apophis30.>
2025-02-17 12:27:37 +0000

    Added trpc config

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { PageClient } from "@/app/(home)/client";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "world" });
+
   return (
-    <div>
-      I will Load Videos In Future!
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>loading...</p>}>
+        <ErrorBoundary fallback={<p>error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { TRPCProvider } from "@/trpc/client";
+
 import "./globals.css";
 
 const inter = Inter({
@@ -20,7 +22,9 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl={"/"}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <TRPCProvider>{children}</TRPCProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
